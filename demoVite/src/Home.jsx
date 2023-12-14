c93925409eb6a761abf3e110a2721fb90edcda23
Wiley Welch <wileyjwelch1@gmail.com>
2023-12-13 23:30:26 -0700

    fixed our bugs to allow for some more help

---
+++ demoVite/src/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+const Home = () => {
+  // const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Add your login logic here
+    console.log('Login button clicked');
+  };
+
+  return (
+    <main>
+      <h2>Welcome</h2>
+      <form className="login-form" method="get">
+        <h3>Login to start</h3>
+        <div className="credits">
+          <div className="user">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" placeholder="Your Username here" required />
+          </div>
+          <div className="pass">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" placeholder="Your Password here" required />
+          </div>
+        </div>
+        <button className="btn btn-primary" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+      <p>
+        Don't have an account?
+        {/* <button onClick={() => navigate('/signup')}>Sign Up Here</button> */}
+      </p>
+    </main>
+  );
+};
+
+export default Home;
