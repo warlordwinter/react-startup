da813c05aece6abf94383c0103268a161a919aa9
Wiley Welch <wileyjwelch1@gmail.com>
2023-12-14 22:16:11 -0700

    We got the signup endpoint to work

--- demoVite/vite.config.js
+++ demoVite/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/signup': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
